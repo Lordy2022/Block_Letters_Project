8ee712328a34e09f5683ac2a8eb64d85fe915279
Kyle Lord <lordkyle@icloud.com>
2023-03-28 22:48:33 +0100

    first commit

---
+++ Block_Letters.py
@@ -0,0 +1,17 @@
+# 1) What is your name, then we can determine your initials: 
+
+# My name is Kyle Lord.
+
+# 2) Give us an interesting fact about yourself!: 
+
+# Most people find it quite interesting, that I have a twin sister.
+
+# 3) For your first Python project, print your name in block letters below:
+
+print("K   K  L     ")  
+print("K  K   L     ")
+print("K K    L     ")
+print("KK     L     ")
+print("K K    L     ")
+print("K  K   L     ")
+print("K   K  LLLLL ")
---
+++ README.md
@@ -0,0 +1,6 @@
+# Block Letters Project
+
+## Part of 'Learn Python 3 - Codecademy Projects'
+
+'ASCII art is a graphic design technique that uses computers for presentation and consists of pictures pieced together from individual characters'. 
+I have Written a Python program that displays the initials of my name in block letters and have now "dipped my toes" into ASCII art.
